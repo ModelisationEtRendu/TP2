6c75ab873dfb5118a5cf3c980e0bfba7c0a52d08
Gutierrez Jorge <jgutierr@localhost.localdomain>
2016-05-12 12:09:08 +0200

    Partie 1

--- Transformation.java
+++ Transformation.java
@@ -23,17 +23,30 @@ public class Transformation  {
 
     public void setLookAt (Vector3 cam, Vector3 lookAt, Vector3 up) {
         try {
-        // compute rotation
+            worldToCamera = Matrix(4, 4);
+            
+		    // compute rotation
+            Vector3 z = lookat.clone();
+            z.substract(cam);
+            z.normalize();
+            Vector3 x = up.cross(z);
+            x.normalize();
+            Vector3 y = z.cross(x);
+            worldToCamera.set(0, 0, x.getX());
+            worldToCamera.set(1, 0, x.getY());
+            worldToCamera.set(2, 0, x.getZ());
+            worldToCamera.set(0, 1, y.getX());
+            worldToCamera.set(1, 1, y.getY());
+            worldToCamera.set(2, 1, y.getZ());
+            worldToCamera.set(0, 2, z.getX());
+            worldToCamera.set(1, 2, z.getY());
+            worldToCamera.set(2, 2, z.getZ());
+            worldToCamera.set(0, 3, cam.getX());
 
-
-	/* à compléter */
-
-
-        // compute translation
-
-        /* à compléter */
-
-
+			// compute translation
+            worldToCamera.set(1, 3, cam.getY());
+            worldToCamera.set(2, 3, cam.getZ());
+            wolrdToCamera.set(3, 3, 1);
 
         } catch (Exception e) { /* unreached */ };
         
@@ -41,19 +54,25 @@ public class Transformation  {
     }
 
     public void setProjection () {
-
-
-	/* à compléter */
-
+		projection = Matrix(3, 4);
+		
+		projection.set(0, 0, 1);
+        projection.set(1, 1, 1);
+        projection.set(2, 2, 1);
+ 
         System.out.println ("Projection matrix:\n" + projection);
     }
 
     public void setCalibration (double focal, double width, double height) {
+		calibration = Matrix(3, 3);
+		
+		calibration.set(0, 0, focal);
+		calibration.set(1, 1, focal);
+		calibration.set(2, 2, 1);
+		calibration.set(0, 2, widht/2);
+		calibration.set(1, 2, height/2);
 
-
-	/* à compléter */
-
-	System.out.println ("Calibration matrix:\n" + calibration);
+        System.out.println ("Calibration matrix:\n" + calibration);
     }
 
     /**
